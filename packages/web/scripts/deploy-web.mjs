38d1a549d88e18f57edbaee824d5b20f830911c2
DoonDoony <cream.doondoon@gmail.com>
2021-12-14 03:03:12 +0900

    :green_heart: Invalidate AWS cloudfront cache after deployment

--- packages/web/scripts/deploy-web.mjs
+++ packages/web/scripts/deploy-web.mjs
@@ -1,6 +1,8 @@
 import { $ } from "zx";
 
 const startMessage = chalk.blueBright('Start web deploying 🚀')
+const uploadToS3Message = chalk.red('Upload build files to AWS S3 ✈️')
+const invalidateCacheMessage = chalk.red('Invalidate cloudfront cache 💣')
 const endMessage = chalk.magenta('Deployment completed successfully 🎉')
 const aws = '/usr/local/bin/aws'
 
@@ -11,7 +13,13 @@ void async function() {
 
   const OUTPUT_DIR = path.join(pwd, '/build')
   const { S3_BUCKET_URI } = process.env
+  const { S3_DISTRIBUTION_ID } = process.env
 
+  console.log(uploadToS3Message)
   await $`${aws} s3 sync ${OUTPUT_DIR} ${S3_BUCKET_URI}`
+
+  console.log(invalidateCacheMessage)
+  await $`${aws} cloudfront create-invalidation --distribution-id ${S3_DISTRIBUTION_ID} --paths "/*"`
+
   console.log(endMessage)
 }();
