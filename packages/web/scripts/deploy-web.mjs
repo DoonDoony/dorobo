9e417ab38e2ea80718d533ceaa94f326ab36e4b4
DoonDoony <cream.doondoon@gmail.com>
2021-12-13 23:56:34 +0900

    :construction_worker: Use Github actions for deployment

---
+++ packages/web/scripts/deploy-web.mjs
@@ -0,0 +1,14 @@
+import { $ } from "zx";
+
+const startMessage = chalk.blueBright('Start web deploying 🚀')
+const endMessage = chalk.magenta('Deployment completed successfully 🎉')
+
+void async function() {
+  console.log(startMessage)
+  const out = await $`pwd`
+  const pwd = out.toString().trim()
+  const OUTPUT_DIR = path.join(pwd, '/build')
+  const { S3_BUCKET_URI } = process.env
+  await $`aws s3 sync ${OUTPUT_DIR} ${S3_BUCKET_URI}`
+  console.log(endMessage)
+}();
