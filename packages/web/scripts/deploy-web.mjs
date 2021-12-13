8245dce4d7220277c3e30ad0ac7ad8363bf7d8c3
DoonDoony <cream.doondoon@gmail.com>
2021-12-14 00:33:05 +0900

    :green_heart: Use aws cli binary via absoulte path

--- packages/web/scripts/deploy-web.mjs
+++ packages/web/scripts/deploy-web.mjs
@@ -2,13 +2,16 @@ import { $ } from "zx";
 
 const startMessage = chalk.blueBright('Start web deploying 🚀')
 const endMessage = chalk.magenta('Deployment completed successfully 🎉')
+const aws = '/usr/local/bin/aws'
 
 void async function() {
   console.log(startMessage)
   const out = await $`pwd`
   const pwd = out.toString().trim()
+
   const OUTPUT_DIR = path.join(pwd, '/build')
   const { S3_BUCKET_URI } = process.env
-  await $`aws s3 sync ${OUTPUT_DIR} ${S3_BUCKET_URI}`
+
+  await $`${aws} s3 sync ${OUTPUT_DIR} ${S3_BUCKET_URI}`
   console.log(endMessage)
 }();
